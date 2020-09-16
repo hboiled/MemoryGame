2ebf3b137d3479c5b65fc9593a52580371155035
Sam Lee <sflee36@yahoo.com.au>
2020-09-16 09:00:00 +0800

    Basic game functionality completed:
    Blocks reveal words when click, words display hidden on failed match, blocks disappear on match, score counter increments

--- src/app/main-game/memory-blocks/block-display/block-display.component.ts
+++ src/app/main-game/memory-blocks/block-display/block-display.component.ts
@@ -1,20 +1,49 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-block-display',
   templateUrl: './block-display.component.html',
   styleUrls: ['./block-display.component.css']
 })
-export class BlockDisplayComponent implements OnInit {
+export class BlockDisplayComponent implements OnInit, OnDestroy {
   
-  @Input() selWord: string = "";
+  @Input() selWord: string = "";  
+  @Input() indexNum: number;
+  @Output() selectedIndex: EventEmitter<number> = new EventEmitter<number>();
+  @Input() setDisplayStatus: Observable<boolean>;
+  @Input() setCompletedStatus: Observable<number[]>;
 
-  constructor() { }
+  displaySubscription: Subscription;
+  completeSubscription: Subscription;
+  displayWord: boolean = false;
+  completed: boolean = false;
+
+  constructor() { }  
 
   ngOnInit(): void {
     console.log(this.selWord);
+    this.displaySubscription = this.setDisplayStatus.subscribe(
+      (status: boolean) => {
+        this.displayWord = status;
+    });
+    this.completeSubscription = this.setCompletedStatus.subscribe(
+      (indexes: number[]) => {
+        // extract indexes to vars
+        if (this.indexNum === indexes[0] || this.indexNum === indexes[1]) {
+          this.completed = true;
+        }
+      }
+    )
   }
 
-  
+  ngOnDestroy(): void {
+    this.displaySubscription.unsubscribe();
+  }
+
+  select(): void {
+    this.displayWord = true;
+    this.selectedIndex.emit(this.indexNum);    
+  }
 
 }
--- src/app/main-game/memory-blocks/memory-blocks.component.ts
+++ src/app/main-game/memory-blocks/memory-blocks.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-memory-blocks',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./memory-blocks.component.css']
 })
 export class MemoryBlocksComponent implements OnInit {
-  
+
+  // game difficulty enum selection
+
   words: string[] = [
     "hello",
     "internet",
@@ -14,13 +17,79 @@ export class MemoryBlocksComponent implements OnInit {
     "friend",
     "computer"
   ];
+  playerScore: number = 0;
+
+  firstSelectedIndex: number = -1;
+  secondSelectedIndex: number = -1;
+
+  displayStatus: Subject<boolean> = new Subject<boolean>();
+  markCompleted: Subject<number[]> = new Subject<number[]>();
+
+  setSelectedIndex(num: number) {
+    //console.log("emitted num: " + num);
+
+    if (this.firstSelectedIndex >= 0) {
+      this.secondSelectedIndex = num;
+    } else {
+      this.firstSelectedIndex = num;
+    }
+    //console.log("first:" + this.firstSelectedIndex);
+    //console.log("sec:" + this.secondSelectedIndex);
+
+    // evaluate both selected indexes
+    if (this.firstSelectedIndex >= 0 && this.secondSelectedIndex >= 0) {
+      this.evaluateSelections();
+      this.turnBackBlocks();
+    }
+
+  }
+
+  evaluateSelections() {
+    console.log(this.words[this.firstSelectedIndex] + " " + this.words[this.secondSelectedIndex])
+    if (this.words[this.firstSelectedIndex] == this.words[this.secondSelectedIndex]) {
+      console.log("match!");
+      setTimeout(() => {
+        this.markCompleted.next([this.firstSelectedIndex, this.secondSelectedIndex]);
+        this.playerScore += 2;
+      }, 1000);
+
+    } else {
+      console.log("no match");
+      this.playerScore--;
+    }
+  }
+
+  resetSelections(): void {
+    this.firstSelectedIndex = -1;
+    this.secondSelectedIndex = -1;
+  }
+
+  turnBackBlocks(): void {
+    /*
+    Logic:
+    default state: all indexes are returned false
+    click event should set block display to true
+    when fsi and ssi are set, set all to false
+    set timeout for 2secs
+    */
+
+    if (this.firstSelectedIndex >= 0 && this.secondSelectedIndex >= 0) {
+
+      setTimeout(() => {
+        this.resetSelections();
+        this.displayStatus.next(false);
+      }, 1000);
+    }
+
+    //return true;
+  }
 
   constructor() { }
 
   ngOnInit(): void {
     this.populateWords();
     this.shuffleArray(this.words)
-    
+
     console.log(this.words);
   }
 
@@ -31,11 +100,11 @@ export class MemoryBlocksComponent implements OnInit {
       this.words.push(cloneArr[i]);
     }
   }
-  
+
   shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
     }
-}
+  }
 }
