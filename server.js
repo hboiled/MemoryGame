03478d84390f582a0292c3eb764a9a13512b9506
Sam Lee <sflee36@yahoo.com.au>
2020-09-20 11:24:01 +0800

    Changes to server.js

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/dist/memory-game'));
+app.use(express.static(__dirname + '/dist/MemoryGame'));
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/memory-game/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/MemoryGame/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
