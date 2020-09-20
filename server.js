072595eb727b8244fcbfaa51c5375664ea993677
Sam Lee <sflee36@yahoo.com.au>
2020-09-20 11:06:29 +0800

    Additional reconfig of deployment settings

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/dist/website'));
+app.use(express.static(__dirname + '/dist/memory-game'));
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/website/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/memory-game/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
